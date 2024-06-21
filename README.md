fc0a2abacd7705f3008caa2ae5bb0941c9c3fb39
GuiFlam <gui.flamand@gmail.com>
2024-06-20 21:27:47 -0400

    finished

--- README.md
+++ README.md
@@ -1,2 +1,29 @@
 # YTToAppleMusic
-Simple python script to convert a youtube video to a mp3 file that can be imported in Apple Music. 
+
+## How to use
+
+1. Clone the repository
+2. Run `pip install -r requirements.txt`
+3. Fill in those values in the `main.py` file
+```python
+video = "https://www.youtube.com/watch?v=dQw4w9WgXcQ"
+title = "NeVeR GoNnA GiVe YoU Up !!!"
+artist = "Rick Astley"
+genre = "Pop"
+```
+Here is the custom song cover I will use for this example:
+<p align="center">
+  <img src="misc/example.jpg" alt="Main Image" width="300">
+</p>
+1. Put the song cover in the `image` folder (make sure it is a `.jpg` file and the aspect ratio is 1:1)
+2. Save the file and run the `main.py`
+3. The script will download the video, convert it to an audio file and add the metadata into the audio file
+
+The next thing to do is to add the song to your Apple Music library. You can do this by opening the Apple Music / iTunes app on your computer and dragging the song into the app. The song will be added to your library and you can listen to it on all your devices.
+
+## Preview 
+
+Here is a preview of the song in my Apple Music library:
+<p align="center">
+  <img src="misc/preview.jpg" alt="Main Image" width="700">
+</p>
---
+++ main.py
@@ -0,0 +1,67 @@
+from pytube import YouTube
+from mutagen.easyid3 import EasyID3
+from moviepy.video.io.VideoFileClip import VideoFileClip
+import eyed3
+from eyed3.id3.frames import ImageFrame
+import os
+
+# Add your video link, title, and artist here
+video = "https://www.youtube.com/watch?v=------------"
+title = "-----------"
+artist = "----------"
+genre = "-----------"
+
+# Define folders
+image_folder = "image/"
+output_folder = "output/"
+
+# Ensure the output folder exists
+if not os.path.exists(output_folder):
+    os.makedirs(output_folder)
+
+# Find the first image file in the image folder
+image_file = None
+for file in os.listdir(image_folder):
+    if file.endswith(".jpg"):
+        image_file = os.path.join(image_folder, file)
+        print("Image file found: " + image_file)
+        break
+
+yt = YouTube(video)
+video = yt.streams.filter().first()
+out_file = video.download(output_path=output_folder)
+
+# Rename the downloaded file to .mp4
+base, ext = os.path.splitext(out_file)
+new_file = base + '.mp4'
+os.rename(out_file, new_file)
+print(yt.title + " has been successfully downloaded.")  
+
+# Extract audio and save as .mp3
+clip = VideoFileClip(new_file)
+mp3_filename = os.path.join(output_folder, f"{title}.mp3")
+clip.audio.write_audiofile(mp3_filename)
+clip.close()
+
+# Remove the .mp4 file
+os.remove(new_file)
+
+# Update audio metadata
+audio = EasyID3(mp3_filename)
+audio['title'] = title
+audio['artist'] = artist
+audio['album'] = title + " - Single"
+audio['genre'] = genre
+audio.save()
+
+# Add cover image to the .mp3 file
+audiofile = eyed3.load(mp3_filename)
+if audiofile.tag is None:
+    audiofile.initTag()
+
+with open(image_file, 'rb') as img_file:
+    audiofile.tag.images.set(ImageFrame.FRONT_COVER, img_file.read(), 'image/jpeg')
+
+audiofile.tag.save()
+
+print("All videos have been processed successfully.")
